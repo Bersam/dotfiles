0fde4f6893a520266aa9bfaa0a4e35187624c57e
Bersam Karbasion <bersam.k@gmail.com>
2014-04-24 20:16:11 +0430

    add

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,11 +1,11 @@
 function fish_prompt
-
+	
   if not set -q -g __fish_robbyrussell_functions_defined
     set -g __fish_robbyrussell_functions_defined
     function _git_branch_name
       echo (git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
     end
-
+	
     function _is_git_dirty
       echo (git status -s --ignore-submodules=dirty ^/dev/null)
     end
@@ -30,5 +30,5 @@ function fish_prompt
     end
   end
 
-  echo -n -s $arrow $cwd $git_info $normal '>'
+  echo -n -s $arrow ' '$cwd $git_info $normal '>'
 end
