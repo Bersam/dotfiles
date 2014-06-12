dd173a4a9e08537b95a772876417172eeb4ea958
Bersam Karbasion <bersam.k@gmail.com>
2014-06-12 23:49:02 +0430

    add auto focus

--- screenlayout/home.sh
+++ screenlayout/home.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-xrandr --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 1600x172 --rotate normal --output VIRTUAL1 --off --output DP1 --off --output VGA1 --mode 1600x900 --pos 0x0 --rotate normal
+
+#!/bin/bash
+
+IN="LVDS1"
+EXT="VGA1"
+HDMI="HDMI1"
+
+if (xrandr | grep "$EXT disconnected"); then
+    xrandr --output $IN --auto --output $EXT --off --output $HDMI --off --output VIRTUAL1 --off
+else
+    xrandr --output $HDMI --off --output $IN --mode 1366x768 --pos 1600x172 --rotate normal --output VIRTUAL1 --off --output DP1 --off --output $EXT --mode 1600x900 --pos 0x0 --rotate normal
+fi
